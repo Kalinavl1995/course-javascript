8ce8b49f39f2700c9cae1ed13d0e2229c763a24e
Kalinin Vladislav <Kalinavl1995@gmail.com>
2022-09-25 14:51:13 +0300

    completed the tasks of the first week

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -1,5 +1,7 @@
 /* ДЗ 1 - Функции */
 
+// import { bind } from "file-loader";
+
 /*
  Задание 1:
 
@@ -12,7 +14,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(value) {}
+function returnFirstArgument(value) {
+  return value;
+}
 
 /*
  Задание 2:
@@ -28,7 +32,9 @@ function returnFirstArgument(value) {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +44,9 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -50,13 +58,17 @@ function returnFnResult(fn) {}
    var f = returnCounter();
    console.log(f()); // выведет 1
    console.log(f()); // выведет 2
-   
+
    f = returnCounter(10);
    console.log(f()); // выведет 11
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+function returnCounter(number = 0) {
+  return function f() {
+    return ++number;
+  };
+}
 
 /*
  Задание 5 *:
@@ -67,7 +79,10 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+  const array = args;
+  return array;
+}
 
 /*
  Задание 6 *:
@@ -84,7 +99,11 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn, ...args) {}
+
+function bindFunction(fn, ...args) {
+  fn = fn.bind(null, ...args);
+  return fn;
+}
 
 export {
   returnFirstArgument,
