5e20499b18abcb54a675d12eb88fd51e59a722cc
Kalinin Vladislav <Kalinavl1995@gmail.com>
2022-10-15 22:42:47 +0300

    completed task 6

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,49 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+homeworkContainer.style.height = '100vh';
 
-export function createDiv() {}
+function getRandom(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+let currentDrag;
+let startY = 0;
+let startX = 0;
+
+document.addEventListener('mousemove', (event) => {
+  if (currentDrag) {
+    currentDrag.style.top = event.clientY - startY + 'px';
+    currentDrag.style.left = event.clientX - startX + 'px';
+  }
+});
+
+export function createDiv() {
+  const newElement = document.createElement('div');
+  const posx = (Math.random() * document.body.offsetWidth).toFixed();
+  const posy = (Math.random() * document.body.offsetHeight).toFixed();
+
+  newElement.classList.add('draggable-div');
+  newElement.style.position = 'absolute';
+  newElement.style.left = `${posx}px`;
+  newElement.style.top = `${posy}px`;
+  newElement.style.width = `${getRandom(70, 150)}px`;
+  newElement.style.height = newElement.style.width;
+  newElement.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(
+    0,
+    255
+  )}, ${getRandom(0, 255)})`;
+
+  newElement.addEventListener('mousedown', (event) => {
+    currentDrag = newElement;
+    startX = event.offsetX;
+    startY = event.offsetY;
+  });
+
+  newElement.addEventListener('mouseup', () => (currentDrag = false));
+
+  return newElement;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
